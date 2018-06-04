52d4d3655175018756dd88ee969f6659e4ab5e83
Jed Peek <jedpeek@yahoo.com>
2018-06-04 20:14:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def batch_badge_creator(arr)
+  badges = []
+  i = 0
+  while i < arr.length
+    badges = "Hello, my name is #{arr[i]}"
+    i+=1
+  end
+  return badges
+end
